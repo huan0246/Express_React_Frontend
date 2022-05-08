60e04e870a656f741fb2af86bf1e37db26220ed5
Huang Chunyen <huan0246@algonquinlive.com>
2022-05-07 22:17:11 -0400

    Fetch repos in useEffect and sort by reverse sequence; Display array data on website

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 export function App() {
+  const [backendData, setBackendData] = useState<any[]>([]);
+
+  useEffect(() => {
+    fetch('/repos')
+      .then((response) => response.json())
+      .then((data) => {
+        data.sort((i: any, j: any) => {
+          if (
+            new Date(i.created_at).getTime() / 1000 >
+            new Date(j.created_at).getTime() / 1000
+          ) {
+            return 1;
+          } else if (
+            new Date(i.created_at).getTime() / 1000 <
+            new Date(j.created_at).getTime() / 1000
+          ) {
+            return -1;
+          } else {
+            return 0;
+          }
+        });
+        data.reverse();
+        setBackendData(data);
+        console.log(data);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {backendData.length > 0 &&
+        // eslint-disable-next-line array-callback-return
+        backendData.map((item) => {
+          return (
+            <div key={item.name + item.id}>
+              <div>{item.name}</div>
+              <div>
+                <div>{item.description}</div>
+                <div>{item.language}</div>
+                <div>{item.forks_count}</div>
+              </div>
+            </div>
+          );
+        })}
+      {backendData.length === 0 && <div>Loading...</div>}
     </div>
   );
 }
